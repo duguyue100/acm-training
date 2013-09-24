4d30d51be69eb4692cdd9e25a65affc895f43705
Hu Yuhuang <duguyue100@gmail.com>
2013-09-24 13:02:39 +0800

    20130924-02
    URAL1335: use relationsihp between numbers.

---
+++ URAL/ural1335.cpp
@@ -0,0 +1,20 @@
+/*
+ * ural1335.cpp
+ *
+ * Use relationship between numbers.
+ *
+ */
+
+#include<iostream>
+
+using namespace std;
+
+int main(void)
+{
+	int n=0;
+	cin >> n;
+
+	cout << n*n+n << " " << n*n+2*n << " " << n*n;
+
+	return 0;
+}
