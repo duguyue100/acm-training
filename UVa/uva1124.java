c7687aba61026f062d0120facf5c0f72291fe41b
Hu Yuhuang <duguyue100@gmail.com>
2013-10-13 21:10:36 +0800

    20131013-02
    UVA 1124: Speechless

---
+++ UVa/uva1124.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class uva1124 {
+	public static void main(String[] args)
+	{
+		Scanner input=new Scanner(System.in);
+		
+		while (input.hasNextLine())
+			System.out.println(input.nextLine());
+	}
+}
