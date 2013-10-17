1eb68925def6bd519a0c72868d59e3d9420bc92f
Hu Yuhuang <duguyue100@gmail.com>
2013-10-18 00:19:37 +0800

    20131018-01
    UVA 12478: ok, I'm not good at grid searching.. And it's a total joke.

---
+++ UVa/uva12478.cpp
@@ -0,0 +1,16 @@
+/*
+ * uva12478.cpp
+ *
+ *  Created on: Oct 18, 2013
+ *      Author: dgyHome
+ */
+
+#include<iostream>
+
+using namespace std;
+
+int main(void)
+{
+	cout << "KABIR" << endl;
+	return 0;
+}
