1151eaf827530908ca91befb5a46314c8b79fa61
Yuhuang Hu <duguyue100@gmail.com>
2016-07-18 20:03:10 +0200

    update 2012

---
+++ URAL/ural2012.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main(void)
+{
+    int N=0;
+    cin >> N;
+
+    if ((12-N)*45>240)
+    {
+        cout << "NO" << endl;
+    }
+    else cout << "YES" << endl;
+
+    return 0;
+}
