fd312990aded75256dd92970dbee3b74f66b83f9
Yuhuang Hu <duguyue100@gmail.com>
2017-06-06 18:42:23 +0200

    update

--- Project_Euler/pe047.py
+++ Project_Euler/pe047.py
@@ -3,3 +3,35 @@
 Author: Yuhuang Hu
 Email : duguyue100
 """
+
+
+def get_prime_factor(n):
+    i = 2
+    factors = []
+    while i * i <= n:
+        if n % i:
+            i += 1
+        else:
+            n //= i
+            if i not in factors:
+                factors.append(i)
+    if n > 1:
+        if n not in factors:
+            factors.append(n)
+    return factors
+
+
+def check(n):
+    for i in xrange(4):
+        if len(get_prime_factor(n+i)) != 4:
+            return False
+    return True
+
+n = 210
+
+while True:
+    if check(n):
+        print n
+        break
+    else:
+        n += 1
