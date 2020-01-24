d2394f70ac3f8f0b1842c7023f0d9e86f61a0f95
Yuhuang Hu <duguyue100@gmail.com>
2020-01-24 14:56:38 +0100

    update uva

---
+++ UVa/uva10583.cpp
@@ -0,0 +1,77 @@
+// UVa 10583 Ubiquitous Religions
+
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <deque>
+#include <set>
+#include <cctype>
+#include <sstream>
+#include <map>
+#include <algorithm>
+#include <utility>
+#include <stack>
+#include <tuple>
+#include <cmath>
+#include <vector>
+#include <bitset>
+
+using namespace std;
+
+class UnionFind {
+private:
+    vector<int> p, rank, setSize;
+    int numSets;
+public:
+    UnionFind(int N) {
+        p.assign(N, 0); for (int i = 0; i < N; ++i) p[i] = i;
+        rank.assign(N, 0);                           // optional speedup
+        setSize.assign(N, 1);                        // optional feature
+        numSets = N;                                 // optional feature
+    }
+
+    int findSet(int i) { return (p[i] == i) ? i : (p[i] = findSet(p[i])); }
+    bool isSameSet(int i, int j) { return findSet(i) == findSet(j); }
+
+    int numDisjointSets() { return numSets; }      // optional
+    int sizeOfSet(int i) { return setSize[findSet(i)]; } // optional
+
+    void unionSet(int i, int j) {
+        if (isSameSet(i, j)) return;                 // i and j are in same set
+        int x = findSet(i), y = findSet(j);          // find both rep items
+        if (rank[x] > rank[y]) swap(x, y);           // keep x 'shorter' than y
+        p[x] = y;                                    // set x under y
+        if (rank[x] == rank[y]) ++rank[y];           // optional speedup
+        setSize[y] += setSize[x];                    // combine set sizes at y
+        --numSets;                                   // a union reduces numSets
+    }
+};
+
+int main()
+{
+    // freopen("input.in", "r", stdin);
+    // freopen("output.out", "w", stdout);
+
+    int n, m;
+
+    int cases = 1;
+    while (cin >> n >> m)
+    {
+        if (n == 0 && m == 0) break;
+
+        UnionFind conn(n);
+
+        for (int i=0; i<m; i++)
+        {
+            int a, b;
+            cin >> a >> b;
+            conn.unionSet(a-1, b-1);
+        }
+
+        cout << "Case " << cases << ": " << conn.numDisjointSets() << endl;
+        cases ++;
+    }
+
+
+    return 0;
+}
