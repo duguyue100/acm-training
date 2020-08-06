076213a44d1e44f6d97777b221eab5d9ab559d04
Yuhuang Hu <duguyue100@gmail.com>
2020-08-06 19:21:10 +0200

    update

---
+++ UVa/uva13025.cpp
@@ -0,0 +1,16 @@
+// UVa 13025 Back to the Past
+
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    // freopen("input.in", "r", stdin);
+    // freopen("output.out", "w", stdout);
+
+    cout << "May 29, 2013 Wednesday" << endl;
+    return 0;
+}
