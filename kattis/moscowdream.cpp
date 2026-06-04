82b14fba511f964f902662a75ebb0523d0621b6f
Yuhuang Hu <duguyue100@gmail.com>
2026-06-05 01:31:08 +0200

    fixing

---
+++ kattis/moscowdream.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Type Aliases for faster typing
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+
+// Macros for cleaner loops and common actions
+#define rep(i, a, b) for (int i = a; i < (b); ++i)
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define fi first
+#define se second
+
+void solve() {
+  // Your problem-solving logic goes here
+  int a, b, c, n;
+  cin >> a >> b >> c >> n;
+
+  if (a < 1 || b < 1 || c < 1 || n < 3) {
+    cout << "NO";
+    return;
+  }
+
+  if (a + b + c >= n) {
+    cout << "YES";
+  } else {
+    cout << "NO";
+  }
+}
+
+int main() {
+  // Fast I/O: Crucial for avoiding Time Limit Exceeded (TLE)
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int t = 1;
+  // cin >> t; // Uncomment if the problem specifies the number of test cases
+  while (t--) {
+    solve();
+  }
+  return 0;
+}
